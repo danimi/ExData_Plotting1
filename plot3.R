c690f1b3f1ebc46723e55f6b4d606e1646ea0034
danimi <danijela.milosevic83@gmail.com>
2015-07-10 16:34:36 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,12 @@
+ds<-read.table("household_power_consumption.txt", header=TRUE,sep=";", stringsAsFactors=FALSE, dec=".")
+d<-ds[ds$Date=="1/2/2007"|ds$Date=="2/2/2007",]
+xdata<-strptime(paste(d$Date, d$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+SubMetering1 <- as.numeric(d$Sub_metering_1)
+SubMetering2 <- as.numeric(d$Sub_metering_2)
+SubMetering3 <- as.numeric(d$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(xdata, SubMetering1, type="l", ylab="Energy sub metering", xlab="")
+lines(xdata, SubMetering2, type="l", col="red")
+lines(xdata, SubMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
